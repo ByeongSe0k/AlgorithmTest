54f9fcaaaee8f23370663bbf74eb3cd22aea829e
ByeongSeokLee <80044597+ByeongSeokLee-Hyeon@users.noreply.github.com>
2023-06-29 00:03:08 +0900

    [unrated] Title: 카운트 다운, Time: 0.02 ms, Memory: 3.68 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181899. 카운트 다운/카운트 다운.cpp
@@ -0,0 +1,14 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(int start, int end) {
+    vector<int> answer;
+    int i = start;
+    while(i >= end){
+        answer.push_back(i);
+        i--;
+    }
+    return answer;
+}
