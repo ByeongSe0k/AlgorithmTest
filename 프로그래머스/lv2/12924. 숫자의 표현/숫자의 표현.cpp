30800ea6bd2f3969b93af12bbb56b376d4b18021
ByeongSeokLee <80044597+ByeongSeokLee-Hyeon@users.noreply.github.com>
2023-07-17 16:50:37 +0900

    [level 2] Title: 숫자의 표현, Time: 0.02 ms, Memory: 3.91 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12924. 숫자의 표현/숫자의 표현.cpp
@@ -0,0 +1,14 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int n) {
+    int answer = 0;
+    int divide = 1;
+    while(divide <= n){
+        if(n % divide == 0) ++answer;
+        divide += 2;
+    }
+    return answer;
+}
